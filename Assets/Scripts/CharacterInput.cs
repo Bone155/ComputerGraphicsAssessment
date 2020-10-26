0569fd78a1d3c8edb274631c2606afce376ff910
Bone155 <s199010@students.aie.edu>
2020-10-25 19:35:27 -0700

    shader work

---
+++ Assets/Scripts/CharacterInput.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Animator animator;
+    public float gravityMulti = 2.0f;
+    public float jumpAmount = 5.0f;
+    bool onGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Forward", Input.GetAxis("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetBool("onGround", onGround);
+
+        if (Input.GetKey(KeyCode.Space) && onGround)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpAmount, rb.velocity.z);
+            onGround = false;
+            animator.applyRootMotion = false;
+        }
+        else
+        {
+            Vector3 extraGravityForce = (Physics.gravity * gravityMulti) - Physics.gravity;
+            rb.AddForce(extraGravityForce);
+        }
+
+        if (!onGround)
+        {
+            animator.SetFloat("Jump", rb.velocity.y);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+        animator.applyRootMotion = true;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        onGround = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        onGround = false;
+    }
+}
