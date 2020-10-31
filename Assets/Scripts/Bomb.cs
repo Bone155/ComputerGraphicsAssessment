f5f784bda21cfa28ddd024b5444006ea8a789b00
Bone155 <s199010@students.aie.edu>
2020-10-30 21:21:02 -0700

    bomb complete

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public float delay = 3f;
+    public float radius = 5f;
+    public float force = 700f;
     public GameObject explosionEffect;
     float countdown;
     bool hasExploded = false;
@@ -28,6 +30,17 @@ public class Bomb : MonoBehaviour
     void Explode()
     {
         Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
 
         Destroy(gameObject);
     }
